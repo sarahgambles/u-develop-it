9c6cbb4c548f6f33bffbaaa74fccd7a626c91884
Sarah Gambles <sarahgambles14@gmail.com>
2020-10-15 14:06:19 -0600

    add post route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const inputCheck = require('./utils/inputCheck');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -57,6 +59,23 @@ app.get('/api/candidate/:id', (req, res) => {
         });
     });
 });
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+    
+    res.json({
+        message: 'success', 
+        data: body,
+        id: this.lastID
+    });
+});
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
@@ -76,15 +95,12 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-    if (err) {
-        console.log(err);
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
     }
-    console.log(result, this.lastID);
 });
 
 // Start server after DB connection
